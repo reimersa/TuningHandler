eaf85a2bfd83a3110e3c27e9823b27ee9d11b8c0
Kyle Cormier <kcormier@physik.uzh.ch>
2021-03-21 12:25:31 +0100

    Add some more reading/writing funcitonality
    Allow overwriting existing files, create a backup
    for safety.

--- test_db.py
+++ test_db.py
@@ -10,43 +10,70 @@ class DBTester(unittest.TestCase):
     test_db_name = 'test_db.json'
 
     def setUp( self ):
+        #create some directories
         if not os.path.exists( self.db_path ):
             os.mkdir( self.db_path )
         self.test_db_full_pathname = os.path.join( self.db_path, self.test_db_name)
 
-    def test_add_data( self ):
-        db = tuning_db.TuningDataFrame() 
-        info = run_tuning.get_all_info_from_logfile( self.test_log_file_name )
-        db.add_data( [ info ] )
+        #make a db from the fake log file 
+        self.info = run_tuning.get_all_info_from_logfile( self.test_log_file_name )
+        self.db1  = tuning_db.TuningDataFrame()
+        self.db1.add_new_data( [ self.info ]  )
 
-        self.assertEqual( db.number_of_entries(), 1 )
+    def test_add_data( self ):
+
+        has_entries = self.db1.number_of_entries() > 0
+        self.assertTrue( has_entries )
 
     def test_write( self ):
 
-        info = run_tuning.get_all_info_from_logfile( self.test_log_file_name )
-        db_1 = tuning_db.TuningDataFrame()
-        db_1.add_data( [ info ] )
-
         if os.path.exists( self.test_db_full_pathname ): 
             os.remove( self.test_db_full_pathname )
-        db_1.write( self.test_db_full_pathname )
+
+        self.db1.write( self.test_db_full_pathname )
         file_is_written = os.path.exists( self.test_db_full_pathname)
 
         self.assertTrue( file_is_written )
 
     def test_write_and_read( self ):
-        info = run_tuning.get_all_info_from_logfile( self.test_log_file_name )
-        db_1 = tuning_db.TuningDataFrame()
-        db_1.add_data( [ info ] )
-        db_1.write( self.test_db_full_pathname )
-        
-        db_2 = tuning_db.TuningDataFrame()
-        db_2.add_from_file( self.test_db_full_pathname )
-        db1_in_db2 = db_1.isin(db_2).all().all()
+        if os.path.exists( self.test_db_full_pathname ):
+            os.remove( self.test_db_full_pathname )
 
-        self.assertTrue( db1_in_db2, f'The saved dbatabase,\n {db_1}\n is not found in the loaded one,\n{db_2}' )
+        self.db1.write( self.test_db_full_pathname )
+        db2 = tuning_db.TuningDataFrame()
+        db2.add_from_file( self.test_db_full_pathname )
+
+        db1_in_db2 = self.db1.isin( db2 ).all().all()
+
+        self.assertTrue( db1_in_db2, f'The saved dbatabase,\n {self.db1}\n is not found in the loaded one,\n{db2}' )
     
-            
+    def test_overwrite( self ):
+        if os.path.exists( self.test_db_full_pathname ):
+            os.remove( self.test_db_full_pathname )
+        self.db1.write( self.test_db_full_pathname )
+
+        db2 = tuning_db.TuningDataFrame() 
+        db2.add_new_data( [ self.info ] )
+        db2.add_from_file( self.test_db_full_pathname )
+        backup_name = db2.overwrite( self.test_db_full_pathname )
+
+        #test that the backup is written
+        backup_exists = os.path.exists( backup_name )
+        self.assertTrue( backup_exists, f'did not find the expected backup file "{backup_name}".' )
+        
+        #test that the backup has the old data
+        backup_db = tuning_db.TuningDataFrame()
+        backup_db.add_from_file( backup_name )
+        backup_is_stored = (backup_db == self.db1 ).all().all()
+        self.assertTrue( backup_is_stored, f'the backup file data \n{backup_db}\n, did not match the expected from original_file \n{self.db1}.' )
+
+        #test that the new file has the new db
+        stored_db = tuning_db.TuningDataFrame()
+        stored_db.add_from_file( self.test_db_full_pathname )
+        new_file_matches_db = ( stored_db == db2 ).all().all()
+        self.assertTrue( new_file_matches_db, f'the new file data \n {stored_db},\n did not match the expected \n {db2}\n' )
+
+        
 
 
 if __name__=='__main__':
--- tuning_db.py
+++ tuning_db.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import os
+import shutil
+
+class FileNotFoundError(Exception):
+    """Raised when a file is expected to exist isn't found."""
+    pass
+    
 
 class TuningDataFrame(pd.DataFrame):
     
@@ -18,13 +24,12 @@ class TuningDataFrame(pd.DataFrame):
     def add_from_file( self, fname ):
         "Read data in from saved file"
         new_df = pd.read_json( fname, orient='records' )
-        new_df['is_from_file'] = True
         self.add_data( new_df )
 
     def add_new_data(self, info ):
         "Add new data which is not saved to file"
         new_df = pd.DataFrame(info)
-        new_df['is_from_file'] = False
+        #new_df['is_from_file'] = False
         self.add_data( new_df )
         
     def add_data( self, info):
@@ -32,11 +37,11 @@ class TuningDataFrame(pd.DataFrame):
         #Maybe faster not to convert if info is already a DataFrame or Pandas Series
         new_df = pd.DataFrame( info )
         new_df['Pos'] = new_df.astype(str)
-        self.__dict__.update( self.append( new_df ).__dict__ )
+        self.__dict__.update( self.append( new_df, ignore_index=True ).__dict__ )
 
-    def without_metadata(self):
-        "return copy without any of the metadata about file persistence"
-        return self.loc[:, self.columns != 'is_from_file' ]
+    #def without_metadata(self):
+    #    "return copy without any of the metadata about file persistence"
+    #    return self.loc[:, self.columns != 'is_from_file' ]
 
     def number_of_entries( self):
         "get the number of entries"
@@ -45,3 +50,24 @@ class TuningDataFrame(pd.DataFrame):
     def write(self, filename):
         "Write the info to json file"
         self.to_json(filename, orient='records')
+
+    def overwrite(self, filename):
+        "overwrite an existing file. Makes a  backup in case. Returns backup name."
+        #store a backup, just in case
+        backup_name = ''
+        if os.path.exists(filename):
+            backup_name = filename.rstrip('.json') + '.backup.json'
+            created_file = shutil.copyfile( filename, backup_name )
+
+            if not os.path.exists(backup_name):
+                raise FileNotFountError(f"tried to create backup file {backup_name} for file {filename}, but {backup_name} not found")
+            os.remove(filename)
+            
+        self.write( filename )
+        return backup_name
+
+    def update_file(self, filename):
+        "update a file, adding new data. Makes a backup, in case. Returns backup name."
+        self.add_from_file( filename )
+        backup_filename = self.overwrite( filename )
+        return  backup_filename
