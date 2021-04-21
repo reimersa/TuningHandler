5609a01fcfd694fd0bd943232ff36a43c0826ddf
Kyle Cormier <kcormier@physik.uzh.ch>
2021-04-21 09:40:11 +0200

    Add ability to do plotsgrid of BER results
    rather than showing a single heatmap, allow a configurable
    grid of heatmaps, where the variables shown on the
    axes of the grid can be passed as an argument to the
    plotting function.

--- plotting.py
+++ plotting.py
@@ -7,7 +7,7 @@ import tuning_db as tdb
 import os
 
 def plot_all_taps_from_scan( db, scan_index, plotdir='plots/test/', 
-                                group_on = ['Module','Chip','TAP0'], cmap = sns.cm.rocket_r):
+                                group_on = ['Module','Chip','TAP0'], cmap = sns.cm.rocket_r, grid=[]):
     '''Plot heatmaps of relevant quantities from a particular BER scan index.
     can choose how to orient between Module, Chip, TAP0, TAP1, TAP2, the three 
     quantities which are "grouped_on" will get be grouped into separate files,
@@ -22,7 +22,6 @@ def plot_all_taps_from_scan( db, scan_index, plotdir='plots/test/',
     pos = df['Pos'].unique()[0]
     ring = df['Ring'].unique()[0]
 
-    f = plt.figure()
 
     pivots = ['TAP0','TAP1','TAP2','Chip','Module']
     for group in group_on:
@@ -30,49 +29,94 @@ def plot_all_taps_from_scan( db, scan_index, plotdir='plots/test/',
             raise ValueError(f'Cannot group on unknown value: {group}. must be one of {pivots}')
         pivots.remove(group)
 
-    for (g1name, g2name, g3name), g3_group in df.groupby(group_on):
+    
+    axes = [None, None]
+    for idx, axis in enumerate(grid):
+        if axis not in group_on:
+            raise ValueError(f'Cannot use {axis} as a grid axis, must be one of the "grouped on" values: {group_on}')
+        if idx > 1:
+            raise ValueError(f'grid can only have two axes (at most columns and rows), but found {grid} with {len(grid)} axes')
+        axes[idx] =  axis 
+    
+    for axis in axes:
+        if not axis is None:
+            group_on.remove(axis)
+        
 
-                    #In principle there can be a different limit for each point shown in the plot
-                    #Might be nice to handle that somehow
-                    min_limit = 1./g3_group['NFrames'].max()
+    for group_values, file_group in df.groupby(group_on):
+        #make sure group_values is always a tuple and not just a string
+        if len(group_on) <= 1:
+            group_values = tuple( [group_values] ) 
 
-                    try:
-                        #If there are multiple entries with the same settings, but different error rates then the pivot won't work
-                        pivot = g3_group.pivot( pivots[0], pivots[1],'Error_Rate')
+        #In principle there can be a different limit for each point shown in the plot
+        #Might be nice to handle that somehow
+        min_limit = 1./file_group['NFrames'].max()
+        param_value_string = ', '.join([ f'{param} = {value}' for param, value in zip(group_on, group_values) ])
+        title = f'{param_value_string}| BER lower limit: {min_limit:.1e}' 
 
-                    except Exception as e:
+        fig = None
+        do_single_plot = ( len(grid) == 0 )
+        if do_single_plot: 
+            fig = plt.figure()
 
-                        #in this case we can recalculate the total Error Rate by summing up the cases appropriately
-                        print(f'Combining mulitple runs for {group_on[0]}={g1name},{group_on[1]}={g2name},{group_on[2]}={g3name}')
-                        print(f'The Original data is:\n {g3_group.to_string()}')
-                        print(f'Calculating combined BER')
-                        g3_group = tdb.combine_ber_measurements( g3_group, pivots)
-                        g3_group['Error_Rate'] = tdb.calculate_bit_error_rates( g3_group )
-                        print(f'The new Data is: \n{g3_group.to_string()}')
-                        pivot = g3_group.pivot( pivots[0], pivots[1], 'Error_Rate')
+            ax = make_ber_heatmap(  pivots, data=file_group, cmap=cmap) 
+            ax.set_title(title)
 
-                    mask  = pivot.isna()
-                    pivot = pivot.fillna(2)
+        else: #do a heatmap grid
+            fg = sns.FacetGrid(file_group, col=axes[0], row=axes[1], margin_titles=True)
+            fg.map_dataframe(make_ber_heatmap, pivots, cmap=cmap)
+            fg.fig.suptitle( title )
+            fg.fig.tight_layout()
+            fig = fg.fig
+            
 
-                    min_val = pivot.values.min()
-                    max_val = 10000000/g3_group['NFrames'].max() #Ph2_ACF reports a maximum of 10M errors, so this is the highest value that will be reported #max(min_val*10, pivot.values.max())
-                    colbar_norm = LogNorm(vmin=min_val, vmax=max_val)
+        param_value_string = '_'.join([ f'{param}.{value}' for param, value in zip(group_on, group_values) ])
+        pltname_base = os.path.join(plotdir, f'BER_{ring}_{param_value_string}')
+        plt.savefig(f'{pltname_base}.png')
+        plt.savefig(f'{pltname_base}.pdf')
 
-                    ax = sns.heatmap(pivot, annot=True, norm=colbar_norm, cmap=cmap, linewidth=0.5, mask= mask )
-                    
-                    title = f'{group_on[0]} = {g1name}, {group_on[1]} = {g2name}, {group_on[2]} = {g3name} | BER lower limit: {min_limit:.1e}' 
-                    ax.set_title(title)
-
-                    pltname_base = os.path.join(plotdir, f'BER_{ring}_{group_on[0]}.{g1name}_{group_on[1]}.{g2name}_pos{pos}_{group_on[2]}.{g3name}')
-
-                    plt.savefig(f'{pltname_base}.png')
-                    plt.savefig(f'{pltname_base}.pdf')
-                    f.clear()
-
+        if fig is not None:
+            fig.clear()
+        
 
 
+
+def make_ber_heatmap(  pivots, **kwargs ):
+    '''make a single Bit Error Rate heatmap object from a dataframe, as a function of the two
+    variables specified by "pivots".'''
+
+    df = kwargs.pop('data')
+
+    try:
+        #If there are multiple entries with the same settings, but different error rates then the pivot won't work
+        pivot = df.pivot( pivots[0], pivots[1],'Error_Rate')
+
+    except Exception as e:
+
+        #in this case we can recalculate the total Error Rate by summing up the cases appropriately
+        print(f'Combining mulitple runs for {group_on[0]}={g1name},{group_on[1]}={g2name},{group_on[2]}={g3name}')
+        print(f'The Original data is:\n {df.to_string()}')
+        print(f'Calculating combined BER')
+        df = tdb.combine_ber_measurements( df, pivots)
+        df['Error_Rate'] = tdb.calculate_bit_error_rates( df )
+        print(f'The new Data is: \n{df.to_string()}')
+        pivot = df.pivot( pivots[0], pivots[1], 'Error_Rate')
+
+    mask  = pivot.isna()
+    pivot = pivot.fillna(2)
+
+    min_val = pivot.values.min()
+    max_val = 10000000/df['NFrames'].max() #Ph2_ACF reports a maximum of 10M errors, so this is the highest value that will be reported #max(min_val*10, pivot.values.max())
+    colbar_norm = LogNorm(vmin=min_val, vmax=max_val)
+
+    ax = sns.heatmap(pivot, annot=True, annot_kws={'fontsize':'xx-small'}, fmt='.0e', norm=colbar_norm, linewidth=0.5, mask= mask, **kwargs )
+    return ax
+
+
+
+           
 if __name__ == '__main__':
 
     import tuning_db as tdb
     db = tdb.TuningDataBase('db/ber.json')
-    plot_all_taps_from_scan(db, 37, group_on=['Module','TAP1','TAP2'])
+    plot_all_taps_from_scan(db, 39, group_on=['Module','Chip','TAP0'], grid=['TAP0','Chip'])
