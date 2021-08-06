a2123f71c05253e513ef01ca250e33de439d566c
Kyle <kcormier@physik.uzh.ch>
2021-08-06 11:18:54 +0200

    WIP: Create Producer class for making data
    In order to separate the data generation step (e.g. running
    a CMSITDAQScan) from the step of controlling the information
    flow (i.e. deciding how the data should be parsed and what
    should be updated, and how the data should be stored). The
    Producer class now does the data "production" part, and the
    "Scan" class still does the data flow part. Names should probably
    be updated.
    
    Also add a simple "LogFileProducer" to act as a dummy which "produces"
    the data without actually having to run a scan. Needs some work, to
    e.g. grab last log file, right now uses one single ber file every time.

--- main.py
+++ main.py
@@ -16,6 +16,42 @@ def run_scan( executer, parsers, updater, persistifier):
     updater.update( scan_output )
     persistifier.update( parsed_data )
 
+class Producer(ABC):
+    """Abstract class which produces some data"""
+
+    @abstractmethod
+    def get_data(self):
+        pass
+
+class CMSITDAQProducer(Producer):
+    """Class for running CMSITDAQ scans, with terminal output saved to a log and returned."""
+
+    def __init__(self, scan_type, xml_file, log_folder):
+        self._type = scan_type
+        self._xml_file = xml_file
+        self._log_folder = log_folder
+
+    def get_data(self):
+        log_file = os.path.join(self._log_folder,f'Run{uu.get_next_runnr()}_{self._type}.log')
+        cmd = f'CMSITminiDAQ -f {self._xml_file} -c {self._type}'
+        log_cmd = f'| tee {log_file}'
+        os.system(f'{cmd} {log_cmd}')
+        return log_file
+
+    def get_log_file(self, runnr=None):
+        if runnr is None:
+            runnr = uu.get_next_runnr()
+        return os.path.join(self._log_folder,f'Run{uu.get_next_runnr()}_{self._type}.log')
+    
+
+class LastLogProducer(CMSITDAQProducer):
+    """WIP: Class for Retrieving scan Log without actually running the scan"""
+
+    def get_data(self):
+        log_file = os.path.join('log','example_ber_R7_mod987654321_chip12_pos200_2000_1800_90000.log')
+        return log_file
+
+
 class Scan(ABC):
     
     @abstractmethod
@@ -39,25 +75,20 @@ class RunUpdateSaveScan(Scan):
         
 
 class CMSITDAQScan(RunUpdateSaveScan):
+    """Class for running a CMSITDAQ scan, updating settings and saving the data."""
 
-    def __init__(self, scan_type, xml_file, 
+    def __init__(self, producer, 
             parsers=[p.Parser([])], 
             updater=s.NothingUpdater(),
             persistifier=l.PrintPersistifier(),
             log_folder='log/'):
-        self._type = scan_type
-        self._xml_file = xml_file
+        self._producer = producer
         self._parsers = parsers
         self._updater = updater
         self._persistifier = persistifier
-        self._log_folder = log_folder
 
     def run_simple(self):
-        log_file = os.path.join(self._log_folder,f'Run{uu.get_next_runnr()}_{self._type}.log')
-        cmd = f'CMSITminiDAQ -f {self._xml_file} -c {self._type}'
-        log_cmd = f'| tee {log_file}'
-        os.system(f'{cmd} {log_cmd}')
-        return log_file
+        return self._producer.get_data()
 
     def run(self):
         log_file = self.run_simple()
@@ -74,6 +105,7 @@ class CMSITDAQScan(RunUpdateSaveScan):
 
 
 class ScanWithMonitoring(CMSITDAQScan):
+    """Class for Scans that requiring an initial CMSITDaq monitoring scan for retrieving some basic info."""
 
     def __init__(self, *args, mon_parsers=[p.Parser([])], **kwargs ):
         super().__init__(*args, **kwargs)
@@ -100,12 +132,11 @@ class ScanWithMonitoring(CMSITDAQScan):
 
 if __name__ == '__main__':
 
-    #execute = CMSITDAQScan( 'pixelalive', 'xml/CMSIT_diskR3_scurve.xml')
+    producer = CMSITDAQProducer('pixelalive','xmlCMSIT_diskR3_scurve.xml','log/')
     parsers = [p.Parser([p.RunNumberGetter('RunNumber')]), p.LogFileParser([p.InitMaskedPixGetter('MaskedPixels')]) ]
     mon_parsers = [ p.LogFileParser([p.VoltageGetter('Voltages'),p.TemperatureGetter('Temps') ]) ]
     updater = s.NothingUpdater()
     persistifier = l.PrintPersistifier()
-    #run_scan( execute, parsers, updater, persistifier)
     
-    my_scan = ScanWithMonitoring('pixelalive','xml/CMSIT_diskR3_scurve.xml',mon_parsers=mon_parsers, parsers=parsers, persistifier=l.PrintPersistifier() )
+    my_scan = ScanWithMonitoring(producer,mon_parsers=mon_parsers, parsers=parsers, persistifier=l.PrintPersistifier() )
     my_scan.run_all()
